58c07003d32d37ec581bbe7c6990c2276a6b9824
Ruter <ruter.lv@gmail.com>
2018-03-08 19:45:07 +0800

    [FIX] change filename

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/components/Index'
 
 Vue.use(Router)
 
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: '小水印',
-      component: HelloWorld
+      component: Index
     }
   ]
 })
