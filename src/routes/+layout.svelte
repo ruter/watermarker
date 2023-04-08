6aa5cd75a508496682d34bd24e0bf97aae854fd0
Ruter Lyu <ruter.lv@gmail.com>
2023-04-08 11:50:20 +0800

    add logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,12 @@
 </svelte:head>
 
 <div class="min-h-full">
-  <nav class="bg-gray-800">
+  <nav class="bg-white">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
           <div class="flex-shrink-0">
-            <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="小水印">
+            <img class="h-24" src="./logo.svg" alt="小水印">
           </div>
         </div>
       </div>
