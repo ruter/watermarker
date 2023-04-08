f7badcb1fe4a8b34f06f012be98d7380679e703e
Ruter Lyu <ruter.lv@gmail.com>
2023-04-08 13:25:20 +0800

    update deta space stuff

---
+++ Discovery.md
@@ -0,0 +1,8 @@
+---
+title: "Watermarker"
+tagline: "A basic offline application that facilitates the addition of watermarks to images."
+theme_color: "#62ba47"
+homepage: "https://deta.space/discovery/@ruter/watermarker"
+---
+
+Watermarker is a highly user-friendly application that enables the addition of watermarks to any image. It can be used completely offline, ensuring no data pertaining to the user's images is uploaded, and all operations are performed within the browser environment.
--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-node";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build', // 输出目录
-			assets: 'build', // 静态资源目录
-			fallback: null, // 404 页面
-			strict: true,
-		}),
-		prerender: {
-			handleMissingId: "ignore",
-		},
-	}
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      handleMissingId: "ignore",
+    },
+  },
 };
 
 export default config;
